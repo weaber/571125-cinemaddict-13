7b4e88e187862717471f58366fbd226f0be1758a
weaber <pnyxer@mail.ru>
2020-12-01 23:43:54 +0300

    Рефакторинг, новая функция для создания элементов

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import {generateFilm} from "./mock/film.js";
 import {getRandomInt} from "./mock/film.js";
 import {generateFilters} from "./mock/filter.js";
 import {createPopupTemplate} from "./view/popup.js";
+import {renderTemplate} from "./utils.js";
 
 const FILMS_AMOUNT = 24;
 
@@ -22,41 +23,37 @@ const MOSTCOMMENTED_CARD_AMOUNT = 2;
 const films = new Array(FILMS_AMOUNT).fill().map(generateFilm);
 const filters = generateFilters(films);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const watchedFilmsAmount = getRandomInt(0, FILMS_AMOUNT);
 
 const siteHeaderElement = document.querySelector(`.header`);
-render(siteHeaderElement, createUserProfileTemplate(watchedFilmsAmount), `beforeend`);
+renderTemplate(siteHeaderElement, createUserProfileTemplate(watchedFilmsAmount), `beforeend`);
 
 const siteMainElement = document.querySelector(`.main`);
-render(siteMainElement, createNavigationTemplate(filters), `beforeend`);
-render(siteMainElement, createSortTemplate(), `beforeend`);
-render(siteMainElement, createFilmsTemplate(), `beforeend`);
+renderTemplate(siteMainElement, createNavigationTemplate(filters), `beforeend`);
+renderTemplate(siteMainElement, createSortTemplate(), `beforeend`);
+renderTemplate(siteMainElement, createFilmsTemplate(), `beforeend`);
 
 const filmsElement = siteMainElement.querySelector(`.films`);
-render(filmsElement, createFilmsListTemplate(), `beforeend`);
+renderTemplate(filmsElement, createFilmsListTemplate(), `beforeend`);
 
 const filmsListElement = filmsElement.querySelector(`.films-list`);
 
 const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
 for (let i = 0; i < Math.min(FILMS_AMOUNT_PER_STEP, FILMS_AMOUNT); i++) {
-  render(filmsListContainerElement, createFilmCardTemplate(films[i]), `beforeend`);
+  renderTemplate(filmsListContainerElement, createFilmCardTemplate(films[i]), `beforeend`);
 }
 
 if (films.length > FILMS_AMOUNT_PER_STEP) {
   let renderedFilmsCount = FILMS_AMOUNT_PER_STEP;
 
-  render(filmsListElement, createShowmoreButtonTemplate(), `beforeend`);
+  renderTemplate(filmsListElement, createShowmoreButtonTemplate(), `beforeend`);
   const showMoreButton = filmsListElement.querySelector(`.films-list__show-more`);
   showMoreButton.addEventListener(`click`, (evt) => {
     evt.preventDefault();
 
     films
       .slice(renderedFilmsCount, renderedFilmsCount + FILMS_AMOUNT_PER_STEP)
-      .forEach((film) => render(filmsListContainerElement, createFilmCardTemplate(film), `beforeend`));
+      .forEach((film) => renderTemplate(filmsListContainerElement, createFilmCardTemplate(film), `beforeend`));
 
     renderedFilmsCount += FILMS_AMOUNT_PER_STEP;
 
@@ -66,19 +63,19 @@ if (films.length > FILMS_AMOUNT_PER_STEP) {
   });
 }
 
-render(filmsElement, createTopratedTemplate(), `beforeend`);
+renderTemplate(filmsElement, createTopratedTemplate(), `beforeend`);
 const topratedContainerElement = filmsElement.querySelector(`.toprated`).querySelector(`.films-list__container`);
 for (let i = 0; i < TOPRATED_CARD_AMOUNT; i++) {
-  render(topratedContainerElement, createFilmCardTemplate(films[i]), `beforeend`);
+  renderTemplate(topratedContainerElement, createFilmCardTemplate(films[i]), `beforeend`);
 }
 
-render(filmsElement, createMostCommentedTemplate(), `beforeend`);
+renderTemplate(filmsElement, createMostCommentedTemplate(), `beforeend`);
 const mostCommentedContainerElement = filmsElement.querySelector(`.mostcommented`).querySelector(`.films-list__container`);
 for (let i = 0; i < MOSTCOMMENTED_CARD_AMOUNT; i++) {
-  render(mostCommentedContainerElement, createFilmCardTemplate(films[i]), `beforeend`);
+  renderTemplate(mostCommentedContainerElement, createFilmCardTemplate(films[i]), `beforeend`);
 }
 
 const footerStatisitcsElement = document.querySelector(`.footer__statistics`);
-render(footerStatisitcsElement, createFooterStatisticsTemplate(films), `beforeend`);
+renderTemplate(footerStatisitcsElement, createFooterStatisticsTemplate(films), `beforeend`);
 
-render(siteMainElement, createPopupTemplate(films[0]), `afterend`);
+// renderTemplate(siteMainElement, createPopupTemplate(films[0]), `afterend`);
---
+++ src/utils.js
@@ -0,0 +1,31 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
+export const getRandomInt = (a = 1, b = 0) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
