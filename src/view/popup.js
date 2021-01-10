def78ee98e7f10502d664f0bc3e4e079b3be0121
weaber <pnyxer@mail.ru>
2021-01-10 21:04:56 +0300

    Сделал экранирование комментария

--- src/view/popup.js
+++ src/view/popup.js
@@ -1,4 +1,5 @@
 import SmartView from "./smart.js";
+import he from "he";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
@@ -125,7 +126,7 @@ const createCommentTemplate = (comment) => {
         <img src=${EmotionPicsMap[emotion]} width="55" height="55" alt="emoji-${emotion}">
       </span>
       <div>
-        <p class="film-details__comment-text">${text}</p>
+        <p class="film-details__comment-text">${he.encode(text)}</p>
         <p class="film-details__comment-info">
           <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${dayjs(date).fromNow()}</span>
