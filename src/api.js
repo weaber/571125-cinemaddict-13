31902fe9e8ed16d2effe8416c0da6119de061006
weaber <pnyxer@mail.ru>
2021-01-13 21:46:38 +0300

    Создал класс для работы API

---
+++ src/api.js
@@ -0,0 +1,68 @@
+const Method = {
+  GET: `GET`,
+  PUT: `PUT`
+};
+
+const SuccessHTTPStatusRange = {
+  MIN: 200,
+  MAX: 299
+};
+
+export default class Api {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getFilms() {
+    return this._load({url: `movies`})
+      .then(Api.toJSON);
+  }
+
+  updateFilm(film) {
+    return this._load({
+      url: `movies/${film.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(film),
+      headers: new Headers({"Content-Type": `application/json`})
+    })
+    .then(Api.toJSON);
+  }
+
+  _load({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers()
+  }) {
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(
+        `${this._endPoint}/${url}`,
+        {method, body, headers}
+    )
+      .then(Api.checkStatus)
+      .catch(Api.catchError);
+
+  }
+
+  checkStatus(response) {
+    if (
+      response.status < SuccessHTTPStatusRange.MIN ||
+      response.status > SuccessHTTPStatusRange.MAX
+    ) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    return response;
+
+  }
+
+  toJSON(response) {
+    return response.json();
+  }
+
+  catchError(err) {
+    throw err;
+  }
+}
